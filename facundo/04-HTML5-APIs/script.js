c369fbbb83138ced67a1b3c73cdda4c6fad1a934
Facundo Suarez <suarezfacundon@gmail.com>
2017-04-21 12:52:02 -0300

    Finished Item 2

--- facundo/04-HTML5-APIs/script.js
+++ facundo/04-HTML5-APIs/script.js
@@ -1,26 +1,50 @@
-document.getElementById("save").addEventListener("click", function () {
-    let textArea = document.getElementById("textArea").value;
-    //mylocalStorage("textArea", textArea);
-    addTextToDatabase(textArea);
+window.addEventListener("load", function () {
+    createDataBase();
+})
+document.getElementById("saveIDB").addEventListener("click", function () {
+    let text = document.getElementById("textArea").value;
+    addText("text", text);
+}, false);
+
+document.getElementById("saveLS").addEventListener("click", function () {
+    mylocalStorage("text", document.getElementById("textArea").value);
+}, false);
+
+document.getElementById("showIDB").addEventListener("click", function () {
+    showText();
+}, false);
+
+document.getElementById("showLS").addEventListener("click", function () {
+    alert("Your saved text in localStorage is: " + localStorage.getItem("text"));
+}, false);
+
+document.getElementById("clearLS").addEventListener("click", function () {
+    localStorage.clear();
+    alert("Text Cleared from localStorage")
+}, false);
+
+document.getElementById("clearIDB").addEventListener("click", function () {
+    clearText();
 }, false);
 
 function mylocalStorage(name, text) {
     localStorage.setItem(name, text);
-    alert("El texto guardado en Local Storage es: " + localStorage.textArea);
+    alert("Text saved in localStorage ");
+
 }
-function openDatabase() {
+function openDatabase(dbName) {
     var openRequest = localDatabase.indexedDB.open(dbName);
     openRequest.onerror = function (e) {
         console.log("Database error: " + e.target.errorCode);
     };
     openRequest.onsuccess = function (event) {
         localDatabase.db = openRequest.result;
+        return localDatabase.db;
     };
 }
-
-function addTextToDatabase(textArea) {
-    var localDatabase = {};
-    var dbName = "text";
+let localDatabase = {};
+function createDataBase() {
+    let dbName = "text";
     localDatabase.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
     localDatabase.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
     localDatabase.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
@@ -28,10 +52,10 @@ function addTextToDatabase(textArea) {
     localDatabase.indexedDB.onerror = function (e) {
         console.log("Database error: " + e.target.errorCode);
     };
-    var deleteDbRequest = localDatabase.indexedDB.deleteDatabase(dbName);
+    let deleteDbRequest = localDatabase.indexedDB.deleteDatabase(dbName);
     deleteDbRequest.onsuccess = function (event) {
         console.log('Database deleted');
-        var openRequest = localDatabase.indexedDB.open(dbName, 1);
+        let openRequest = localDatabase.indexedDB.open(dbName, 1);
 
         openRequest.onerror = function (e) {
             console.log("Database error: " + e.target.errorCode);
@@ -39,43 +63,83 @@ function addTextToDatabase(textArea) {
         openRequest.onsuccess = function (event) {
             console.log("Database created");
             localDatabase.db = openRequest.result;
-            var transaction = localDatabase.db.transaction("text", "readwrite");
-            var store = transaction.objectStore("text");
-            var request = store.put({
-                "id": "1",
-                "text": textArea
-            });
-            request.onsuccess = function (e) {
-                alert("Text Added");
-            };
-            try {
-                if (localDatabase != null && localDatabase.db != null) {
-                    var store = localDatabase.db.transaction("text").objectStore("text");
-                    store.get("1").onsuccess = function (event) {
-                        var text = event.target.result;
-                        if (text == null) {
-                            result.innerHTML = "text not found";
-                        }
-                        else {
-                            console.log(text);
-                            alert("Your text is: " + text.text);
-                        }
-                    };
-                }
-            }
-            catch (e) {
-                console.log(e);
-            }
+            return localDatabase.db;
+
+
         };
         openRequest.onupgradeneeded = function (evt) {
             console.log('Creating object stores');
-            var textStore = evt.currentTarget.result.createObjectStore
+            let textStore = evt.currentTarget.result.createObjectStore
                 ("text", { keyPath: "id" });
         };
         deleteDbRequest.onerror = function (e) {
             console.log("Database error: " + e.target.errorCode);
         };
 
+    };
+}
+
+function addText(dbName, textArea) {
+    let openRequest = localDatabase.indexedDB.open(dbName, 1);
+    openRequest.onsuccess = function (event) {
+        localDatabase.db = openRequest.result;
+    };
+    openRequest.onerror = function (e) {
+        console.log("Database error: " + e.target.errorCode);
+    };
+    let transaction = localDatabase.db.transaction("text", "readwrite");
+    let store = transaction.objectStore("text");
+    let request = store.put({
+        "id": "1",
+        "text": textArea
+    });
+    request.onsuccess = function (e) {
+        alert("Text Added");
+    };
+
+}
+function showText(dbName) {
+    let openRequest = localDatabase.indexedDB.open(dbName, 1);
+    openRequest.onsuccess = function (event) {
+        console.log("Database Open");
+        localDatabase.db = openRequest.result;
+    };
+    openRequest.onerror = function (e) {
+        console.log("Database error: " + e.target.errorCode);
+    };
+    let store = localDatabase.db.transaction("text").objectStore("text");
+    store.get("1").onsuccess = function (event) {
+        let text = event.target.result;
+        if (text == null) {
+            result.innerHTML = "text not found";
+        }
+        else {
+            console.log(text);
+            alert("Your text is: " + text.text);
+        }
+    };
+}
+function clearText() {
+    let dbName = "text";
+    let openRequest = localDatabase.indexedDB.open(dbName, 1);
+    openRequest.onsuccess = function (event) {
+        localDatabase.db = openRequest.result;
+        var store = localDatabase.db.transaction("text", "readwrite").objectStore("text");
+        var request = store.openCursor(IDBKeyRange.only("1"));
+
+        request.onsuccess = function () {
+            var cursor = request.result;
+
+            if (cursor) {
+                cursor.delete();
+                cursor.continue();
+            }
+            
+        };
+        alert("Text Cleared in Data Base");
+    };
+    openRequest.onerror = function (e) {
+        console.log("Database error: " + e.target.errorCode);
     };
 
 }
