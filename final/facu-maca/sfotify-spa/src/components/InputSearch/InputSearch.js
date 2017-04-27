fb3c0dc5c76d7dd8f01410b15d724416ecf03486
Facundo Suarez <suarezfacundon@gmail.com>
2017-04-27 14:32:38 -0300

    funciona

--- final/facu-maca/sfotify-spa/src/components/InputSearch/InputSearch.js
+++ final/facu-maca/sfotify-spa/src/components/InputSearch/InputSearch.js
@@ -7,7 +7,6 @@ class InputSearch extends Component {
       <form method="get" action="/">
         <label htmlFor="txtArtist">Search for an artist</label>
         <input type="text" name="txtArtist" id="txtArtist" placeholder="Search the name of your favorite artist" />
-
         <input type="submit" value="Search" />
       </form>
     )
